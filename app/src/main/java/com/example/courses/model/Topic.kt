2b4397594ed4bfe50dd5989256aa4950c6cb2c4f
yasmine mansour <yasminemansour2003@gmail.com>
2024-12-04 13:55:55 +0100

    first commit

---
+++ app/src/main/java/com/example/courses/model/Topic.kt
@@ -0,0 +1,11 @@
+
+package com.example.courses.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Topic(
+    @StringRes val name: Int,
+    val availableCourses: Int,
+    @DrawableRes val imageRes: Int
+)
